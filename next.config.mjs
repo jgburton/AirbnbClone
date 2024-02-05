551d6c498a17d4f81af48bf0889ce90c5adbc81a
Jamie Thomas Grantham-Burton <jgranthamburton@Jamies-MacBook-Air>
2024-02-05 17:36:04 +1100

    github authentication working

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // TODO: Breaks, perhaps not needed in new nextJS ?
-    // images: {
-    //     domains: [
-    //         "avatars.githubusercontent.com",
-    //         "lh3.googleusercontent.com"
-    //     ]}
+    images: {
+        domains: [
+            "avatars.githubusercontent.com", 
+            "lh3.googleusercontent.com"
+        ]}
 };
 
 export default nextConfig;
